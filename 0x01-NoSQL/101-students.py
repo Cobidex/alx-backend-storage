a4b2a7e29b4957913f844bd46dbd6a47d849b40b
Stephen Obialor <stevobialor@gmail.com>
2023-05-09 20:02:45 +0000

    done

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,17 +1,24 @@
+#!/usr/bin/env python3
+
+""" This script uses pymongo to perform MongoDB operations in Python """
+
+from pymongo import MongoClient
+
 def top_students(mongo_collection):
-    pipeline = [
+    """
+    Returns a sorted list of students based on their average score
+
+    :param mongo_collection: pymongo collection object
+    :return: a list of dictionaries containing the name and average score of each student
+    """
+    top_students = mongo_collection.aggregate([
         {
-            '$project': {
-                'name': 1,
-                'averageScore': {
-                    '$avg': '$topics.score'
-                }
+            "$project": {
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
             }
         },
-        {
-            '$sort': {
-                'averageScore': -1
-            }
-        }
-    ]
-    return list(mongo_collection.aggregate(pipeline))
+        {"$sort": {"averageScore": -1}}
+    ])
+
+    return top_students
