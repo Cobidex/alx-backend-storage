078021d6be3ef829b39fa084d9aea3f021cc9c5c
Stephen Obialor <stevobialor@gmail.com>
2023-05-11 00:41:23 +0000

    done

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,44 +1,37 @@
 #!/usr/bin/env python3
-""" Redis Module: This script demonstrates how to cache and count HTTP requests using Redis. """
+"""Web cache and tracker using Redis"""
 
-from functools import wraps
 import redis
 import requests
-from typing import Callable
 
-# Create a Redis instance
-redis_ = redis.Redis()
+# Create a Redis client instance
+r = redis.Redis()
 
-# Define a decorator to count the number of requests made to a specific URL
-def count_requests(method: Callable) -> Callable:
-    """ This decorator counts the number of requests made to a URL """
-    @wraps(method)
-    def wrapper(url):  
-        """ This wrapper function is used to count the requests and cache the results """
-        # Increment the count for the given URL
-        redis_.incr(f"count:{url}")
-        
-        # Check if the result for the given URL is already cached in Redis
-        cached_html = redis_.get(f"cached:{url}")
-        if cached_html:
-            # Return the cached HTML
-            return cached_html.decode('utf-8')
-        
-        # If the result is not cached, make a request to the URL
-        html = method(url)
-        
-        # Cache the result for 10 seconds
-        redis_.setex(f"cached:{url}", 10, html)
-        
-        # Return the HTML
-        return html
-
-    return wrapper
+# Set a variable to keep track of URL counts
+count = 0
 
 
-# Define a function to obtain the HTML content of a URL
-@count_requests
 def get_page(url: str) -> str:
-    """ This function obtains the HTML content of a given URL """
-    req = requests.get(url)
-    return req.text
+    """Obtain the HTML content of a given URL and track how many times it is accessed
+       by incrementing a count stored in Redis with key 'count:{url}'
+       Cache the result with an expiration time of 10 seconds using Redis 'setex'
+       Return the HTML content of the URL"""
+    
+    # Store the count for the given URL in Redis
+    r.set(f"cached:{url}", count)
+    
+    # Send a GET request to the URL
+    resp = requests.get(url)
+    
+    # Increment the count for the URL in Redis
+    r.incr(f"count:{url}")
+    
+    # Cache the HTML content in Redis with an expiration time of 10 seconds
+    r.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
+    
+    # Return the HTML content of the URL
+    return resp.text
+
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
