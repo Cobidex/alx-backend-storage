7edc2a27d21bac615c1fc8cf93e57a0f85fdb0de
Stephen Obialor <stevobialor@gmail.com>
2023-05-11 00:21:04 +0000

    done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,6 +2,7 @@
 """
 Cache module
 """
+from functools import wraps
 import redis
 import uuid
 from typing import Union, Callable
@@ -48,3 +49,44 @@ class Cache:
         Retrieves the integer value for the given key from Redis
         """
         return self.get(key, fn=int)
+
+    def count_calls(self, func: Callable) -> Callable:
+        """
+        count how many times methods of the Cache class are called
+        """
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            self._redis.incr(func.__qualname__)
+            return func(self, *args, **kwargs)
+        return wrapper
+
+    def call_history(method):
+        """
+        store the history of inputs and outputs for a particular function
+        """
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        inputs_key = "{}:inputs".format(method.__qualname__)
+        outputs_key = "{}:outputs".format(method.__qualname__)
+        self._redis.rpush(inputs_key, str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(outputs_key, output)
+        return output
+    return wrapper
+
+    def replay(func):
+        """
+        display the history of calls of a particular function
+        """
+    inputs_key = "{}:inputs".format(func.__qualname__)
+    outputs_key = "{}:outputs".format(func.__qualname__)
+    inputs = cache._redis.lrange(inputs_key, 0, -1)
+    outputs = cache._redis.lrange(outputs_key, 0, -1)
+    num_calls = len(inputs)
+
+    print("{} was called {} times:".format(func.__qualname__, num_calls))
+
+    for i, (input_str, output_str) in enumerate(zip(inputs, outputs)):
+        input_args = eval(input_str.decode("utf-8"))  # Convert string back to tuple
+        output = output_str.decode("utf-8")
+        print("{}(*{}) -> {}".format(func.__qualname__, input_args, output))
