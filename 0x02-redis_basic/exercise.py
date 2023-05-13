d43920c277e4c064909c8a1704ebf1d240f87d4f
Stephen Obialor <stevobialor@gmail.com>
2023-05-13 18:34:02 +0000

    correct pycodestyle indentation error

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -64,7 +64,7 @@ def replay(method: Callable) -> None:
     outputs = cache.lrange(f'{name}:outputs', 0, -1)
     for i, o in zip(inputs, outputs):
         print("{}(*{}) -> {}". format(name, i.decode('utf-8'),
-                                     o.decode('utf-8')))
+                                      o.decode('utf-8')))
 
 
 class Cache:
