76d6af27bf39460e9795e8adb480e2fc8d673a9d
Stephen Obialor <stevobialor@gmail.com>
2023-05-13 19:31:24 +0000

    implement decorator for count

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -3,12 +3,30 @@
 
 import redis
 import requests
+from functools import wraps
 
 r = redis.Redis()
-
 count = 0
 
 
+def count_access(method: Callable) -> Callable:
+    """
+    decorator method to get count of number of times url is assessed
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        wrapper function around the method
+        """
+        url = args[0]
+        r.setnx(f"count:{url}", count)
+        r.incr(f"count:{url}")
+        r.setex(f"count:{url}", 10, r.get(f"count:{url}"))
+        return method(*args, **kwargs)
+    return wrapper
+
+
+@count_access
 def get_page(url: str) -> str:
     """
     Obtain the HTML content of a given URL and track how many
@@ -19,10 +37,7 @@ def get_page(url: str) -> str:
     using Redis 'setex'
     Return the HTML content of the URL
     """
-    r.setnx(f"count:{url}", count)
     resp = requests.get(url)
-    r.incr(f"count:{url}")
-    r.setex(f"count:{url}", 10, r.get(f"count:{url}"))
     return resp.text
 
 
