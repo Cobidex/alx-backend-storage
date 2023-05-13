a859eb3f56fa4b87035bf5f146c6e70d50af3cbc
Stephen Obialor <stevobialor@gmail.com>
2023-05-13 18:32:02 +0000

    correct replay method to give desired output

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -59,11 +59,11 @@ def replay(method: Callable) -> None:
     name = method.__qualname__
     cache = redis.Redis()
     calls = cache.get(name).decode("utf-8")
-    print("{} was called {} times:".format(name, calls))
-    inputs = cache.lrange(name + ":inputs", 0, -1)
-    outputs = cache.lrange(name + ":outputs", 0, -1)
+    print("{} was called {} times:". format(name, calls))
+    inputs = cache.lrange(f'{name}:inputs', 0, -1)
+    outputs = cache.lrange(f'{name}:outputs', 0, -1)
     for i, o in zip(inputs, outputs):
-        print("{}(*{}) -> {}".format(name, i.decode('utf-8'),
+        print("{}(*{}) -> {}". format(name, i.decode('utf-8'),
                                      o.decode('utf-8')))
 
 
